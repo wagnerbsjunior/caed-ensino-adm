af2bb81b436b1228c5711757913f599412c3ada4
wagnerbsjunior <wagnerjf@gmail.com>
2021-02-17 08:11:26 -0300

    Configurando o template css e componentes de navegação

---
+++ ensino-adm-app-vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-navbar/>  
+</template>
+
+<script>
+import Navbar from '@/components/template/Navbar.vue'
+
+export default {
+  components: { 'app-navbar': Navbar }
+}
+</script>
+
+<style>
+  @import './assets/styles.css';
+</style>
